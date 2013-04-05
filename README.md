2f8e4980757fb69337fc315fbade49fb74965e1a
Kiran Koduru <kk@energysolforum.com>
2013-04-05 15:38:30 -0400

    Using Zend Google Data API to insert event

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Google Calendar API problems
+
 Since we need the *service* class to return *true* we'll have to set objects true for it.
 
 	$client->setUseObjects(true);
@@ -8,3 +9,25 @@ Since we need the *service* class to return *true* we'll have to set objects tru
 	// cause the default behaviour is to return arrays
 
 > Answer for the Stack Overflow Question [here](http://stackoverflow.com/questions/11908420/trying-to-get-a-list-of-events-from-a-calendar-using-php)
+
+Instantiate a class with new *Google_* prefix
+
+	$calendar = new Google_Calendar();
+
+
+# Install the Zend Framework install for parsing GData
+
+save in the htaccess of the root file
+php_value include_path "../ZendGdata-1.12.3/library"
+
+
+save as PHP
+top 
+
+load Zend Gdata libraries
+Zend Gdata 
+Zend Gdata Query
+Zend client login
+
+
+Zend Gdata Query
--- index.php
+++ index.php
@@ -2,12 +2,16 @@
 session_start();
 require_once 'Google_Client.php';
 require_once 'Google_CalendarService.php';
+require_once '../googlecalendar/ZendGdata-1.12.3/library/Zend/Loader.php' ;
+
+// Inorder to use the Google Data API for inserting events into calendar
+ZendLoader::loadClass('Zend_Gdata');
+ZendLoader::loadClass('Zend_Gdata_Query');
 
 $scriptUri = "http://".$_SERVER["HTTP_HOST"].$_SERVER['PHP_SELF'];
 
 $client = new Google_Client();
 $client->setAccessType('online'); // default: offline
-$client->setApplicationName('My Application name');
 $client->setClientId('615935043870.apps.googleusercontent.com');
 $client->setClientSecret('66FOV9zJXZ__gYyiZZwUq_I-');
 $client->setRedirectUri($scriptUri);
@@ -17,6 +21,10 @@ $client->setApplicationName("Calendar");
 // $service implements the client interface, has to be set before auth call
 $client->setUseObjects(true); 
 $service = new Google_CalendarService($client);
+
+
+// create instance of Calendar service via Gdata
+$gdata_service = new Zend_Gdata_Calendar($client);
 
 if (isset($_GET['logout'])) { // logout: destroy token
     unset($_SESSION['token']);
@@ -43,28 +51,42 @@ if (!$client->getAccessToken()) { // auth call to google
 
 
 try {
-//energysolutionsforum.org_n68es4rrhp273jv7vcrg5ik9r4@group.calendar.google.com
-	$events = $service->events->listEvents('energysolutionsforum.org_n68es4rrhp273jv7vcrg5ik9r4@group.calendar.google.com');
-	foreach ($events->getItems() as $event ) {
 
-		echo "<pre>";
-		print_r($event);
-		//exit();
-	}
-		
+
+	/* param for XMl
+
+	@email - Email Address
+	@rel - relationship
+	gd:attendeeStaute - gEnumConstruct - Status of event attendee
+	gd:attendeeType - gEnumConstruct - Type of event attendee
+	gd:EntryLink - EntryLink - Entry representing person details. This entry should implement the Contact kind. In many cases, this entry will come from a contact feed.
 	
-	// while(true) {
-	//   foreach ($events->getItems() as $event) {
-	//     print_r($event) ;
-	//   }
-	//   $pageToken = $events->getNextPageToken();
-	//   if ($pageToken) {
-	//     $optParams = array('pageToken' => $pageToken);
-	//     $events = $service->events->listEvents('primary', $optParams);
-	//   } else {
-	//     break;
-	//   }
-	// }
+
+	eg : <gd:who rel="http://schemas.google.com/g/2005#message.from" email="jo@example.com"/>
+	*/
+
+	// $event = new Google_Event();
+	// $event->setSummary('Angeliques Event');
+	// $event->setLocation('137 Varick Street, New York, NY 10013');
+	// $start = new Google_EventDateTime();
+	// $start->setDateTime('2011-06-03T10:00:00.000-07:00');
+	// $event->start($setStart);
+	// $end = new Google_EventDateTime();
+	// $end->setDateTime('2011-06-03T10:25:00.000-07:00');
+	// $event->setEnd($end);
+	// $attendee1 = new Google_EventAttendee();
+	// $attendee1->setEmail('kk@energysolforum.com');
+	// $attendee2 = new Google_EventAttendee();
+	// $attendee2->setEmail('vg@energysolforum.com');
+	// // ...
+	// $attendees = array($attendee1,$attendee2);
+	// $event->attendees = $attendees;
+	// $createdEvent = $service->events->insert('primary', $event);
+
+	// echo $createdEvent->getId();
+	
+	$listFeed = $gdata_service->getCalendarListFeed();
+
     
 } catch (Exception $e) {
     die('An error occured: ' . $e->getMessage()."\n");
